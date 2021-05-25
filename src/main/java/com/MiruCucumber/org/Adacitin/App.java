67b771e626d81bd27e21a25235e8cd080f7b9429
Miruvignesh <mirunalini0506@gmail.com>
2021-05-25 18:30:58 +0530

    first commit

---
+++ src/main/java/com/MiruCucumber/org/Adacitin/App.java
@@ -0,0 +1,13 @@
+package com.MiruCucumber.org.Adacitin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
